06e70a2f537b2527f1db21ca70c92abba5a44978
Santiago956 <ppfsantiago@gmail.com>
2026-05-31 20:19:37 -0300

    Implementando interface do repositório

---
+++ src/repository/interface/interface_quality_rule_repository.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+
+from src.model.quality_rule_model import QualityRuleModel
+
+class IQualityRuleRepository:
+    """
+    Interface for QualityRuleRepository, defining the contract for CRUD operations on quality rules."""
+    
+    def create(self,
+               rule_type: str,
+               target_table: str,
+               target_column: str,
+               min_value: float | None = None,
+               max_value: float | None = None,
+               enum_values: list[str] | None = None,
+               regex_pattern: str | None = None) -> QualityRuleModel:
+        """
+        Create a new quality rule with the specified parameters.
+        """
+        
+        pass
+    
+    def read(self,
+             rule_id: int) -> QualityRuleModel | None:
+        """"
+        Read a quality rule by its ID.
+        """
+        pass
+    
+    def read_by_target_table(self,
+                             target_table: str,
+                             is_active: bool | None = True) -> list[QualityRuleModel]:
+        """"
+        Read quality rules by their target table.
+        """
+        pass
+    
+    def update(self,
+               rule_id: int,
+               new_rule_data: dict) -> QualityRuleModel:
+        """"
+        Update a quality rule with the specified parameters.
+        """
+        pass
+    
+    def delete(self,
+               rule_id: int) -> QualityRuleModel:
+        """"
+        Delete a quality rule by its ID.
+        """
+        pass
+    
+    def revert_delete(self,
+               rule_id: int) -> QualityRuleModel:
+        """"
+        Revert the deletion of a quality rule by its ID.
+        """
+        pass
